cd23f9df1a44b410d174d206a5a9569272e86f21
tusharsonthalia <sonthaliatushar@gmail.com>
2020-12-24 14:39:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
