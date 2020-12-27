81002da88b007c1bb65fd66216a828296cf15b7b
tusharsonthalia <sonthaliatushar@gmail.com>
2020-12-27 16:24:15 +0530

    tidied up the code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import data from './data.json';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import data from "./data.json";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <nav className="navbar text justify-content-around">
-          <ul className="nav">
-            <li className="nav-link">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/resume">Resume</Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/about">About</Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/contact">Contact</Link>
-            </li>
+        <nav className="navbar p-0 text bg-dark justify-content-around font-weight-bold">
+          <ul className="nav text-dark">
+              <Link to="/">
+                <p className="nav-link">Home</p>
+              </Link>
+              <Link to="/resume">
+                <p className="nav-link">Resume</p>
+              </Link>
+              <Link to="/about">
+                <p className="nav-link">About</p>
+              </Link>
+              <Link to="/contact">
+                <p className="nav-link">Contact</p>
+              </Link>
           </ul>
         </nav>
-        <Route exact path="/" component="Home" />
-        <Route path="/resume" component="Resume" />
-        <Route path="/about" component="About" />
-        <Route path="/contact" component="Contact" />
+        <Route exact path="/" component={Home} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </Router>
     );
   }
